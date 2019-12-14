26dc2dffea9a2049abe4201b25089d10d55ee947
Maik Macho <nuda1998@gmail.com>
2019-12-14 20:07:47 +0100

    Don't load luacom outside of Windows

--- src/luacom.lua
+++ src/luacom.lua
@@ -1,13 +1,18 @@
+local jit = _G.jit
+if jit and jit.os ~= "Windows" then
+    return false
+end
+
 -- LuaCOM dumps itself into _G
 
 if not _G.luacom then
-  local init, err1, err2 = package.loadlib("luacom.dll", "luacom_openlib")
-  if not init then
-    print((err1 or "") .. (err2 or ""))
-    return false
-  end
+    local init, err1, err2 = package.loadlib("luacom.dll", "luacom_openlib")
+    if not init then
+        print(err1, err2)
+        return false
+    end
 
-  init()
+    init()
 end
 
 return _G.luacom
