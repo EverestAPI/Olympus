a96d5caf55305100a46328136e2d67459d2e66a4
Maik Macho <nuda1998@gmail.com>
2020-03-12 13:42:18 +0100

    Update OlympUI, "benchmark" scene

--- src/scenes/benchmark.lua
+++ src/scenes/benchmark.lua
@@ -8,17 +8,57 @@ local scene = {}
 local root = uie.column({
     uie.image("header"),
 
-    uie.scrollbox(
-        uie.list(
-            uiu.map(uiu.listRange(2000, 1, -1), function(i)
-                return { text = string.format("%i%s", i, i % 7 == 0 and " (something)" or ""), data = i }
-            end)
-        ):with({
-            grow = false
-        }):with(uiu.fillWidth):with(function(list)
-            list.selected = list.children[1]
-        end):as("versions")
-    ):with(uiu.fillWidth):with(uiu.fillHeight(true)),
+    uie.row({
+
+        uie.scrollbox(
+            uie.list(
+                uiu.map(uiu.listRange(2000, 1, -1), function(i)
+                    return { text = string.format("%i%s", i, i % 7 == 0 and " (something)" or ""), data = i }
+                end)
+            ):with({
+                grow = false
+            }):with(uiu.fillWidth):with(function(list)
+                list.selected = list.children[1]
+            end):as("versions")
+        ):with(uiu.fillWidth(4.25)):with(uiu.fillHeight),
+
+        uie.scrollbox(
+            uie.list(
+                uiu.map(uiu.listRange(2000, 1, -1), function(i)
+                    return { text = string.format("%i%s", i, i % 7 == 0 and " (something)" or ""), data = i }
+                end)
+            ):with({
+                grow = false
+            }):with(uiu.fillWidth):with(function(list)
+                list.selected = list.children[1]
+            end):as("versions")
+        ):with(uiu.fillWidth(4.25)):with(uiu.fillHeight):with(uiu.at(0.25 + 8)),
+
+        uie.scrollbox(
+            uie.list(
+                uiu.map(uiu.listRange(2000, 1, -1), function(i)
+                    return { text = string.format("%i%s", i, i % 7 == 0 and " (something)" or ""), data = i }
+                end)
+            ):with({
+                grow = false
+            }):with(uiu.fillWidth):with(function(list)
+                list.selected = list.children[1]
+            end):as("versions")
+        ):with(uiu.fillWidth(4.25)):with(uiu.fillHeight):with(uiu.at(0.5 + 8)),
+
+        uie.scrollbox(
+            uie.list(
+                uiu.map(uiu.listRange(2000, 1, -1), function(i)
+                    return { text = string.format("%i%s", i, i % 7 == 0 and " (something)" or ""), data = i }
+                end)
+            ):with({
+                grow = false
+            }):with(uiu.fillWidth):with(function(list)
+                list.selected = list.children[1]
+            end):as("versions")
+        ):with(uiu.fillWidth(0.25)):with(uiu.fillHeight):with(uiu.at(0.75 + 8)),
+
+    }):with(uiu.fillWidth):with(uiu.fillHeight(true)),
 
 })
 scene.root = root
