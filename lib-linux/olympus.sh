f42ecfce3d20ffca8e9484d22e10e345ca855635
Maik Macho <nuda1998@gmail.com>
2020-12-19 15:21:39 +0100

    Update Linux olympus.sh to take love2d alias into account

--- lib-linux/olympus.sh
+++ lib-linux/olympus.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 cd $(dirname "$0")
-love --fused olympus.love
+if command -v love &> /dev/null; then
+    love --fused olympus.love
+elif command -v love2d &> /dev/null; then
+    love2d --fused olympus.love
+else
+    echo "love2d not found!"
+fi
