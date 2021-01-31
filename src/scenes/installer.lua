f0b52d33ea6bad1c33a967d47741248c069858ec
Jade Macho <nuda1998@gmail.com>
2021-01-31 19:45:22 +0100

    Move installer shape down just a bit

--- src/scenes/installer.lua
+++ src/scenes/installer.lua
@@ -196,7 +196,7 @@ local root = uie.group({
             end
         end
 
-    }):with(uiu.fillWidth):with(uiu.fillHeight(0.5 - 120)):as("canvas"),
+    }):with(uiu.fillWidth):with(uiu.fillHeight(0.5 - 180)):as("canvas"),
 
     uie.group({
         uie.column({
