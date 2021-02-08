857fc7dafe932218a0485934a63be1ad453affb1
Jade Macho <nuda1998@gmail.com>
2021-02-08 23:38:26 +0100

    Split Linux installation from modinstaller.lua into install.sh

---
+++ lib-linux/install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Olympus install script bundled with Linux builds.
+
+SRCDIR="$(dirname "$(realpath "$0")")"
+cd "$SRCDIR" || exit 1
+
+echo "Olympus exists at $SRCDIR"
+echo "For a full installation experience, this script will set up the following:"
+
+DESKTOPSRCFILE="$SRCDIR/olympus.desktop"
+BINSRCFILE="$SRCDIR/olympus.sh"
+ICONSRCFILE="$SRCDIR/olympus.png"
+
+if [ "$EUID" -ne 0 ]; then
+    if [ -n "$XDG_DATA_HOME" ]; then
+        APPSDIR="$XDG_DATA_HOME/applications"
+    else
+        APPSDIR="$HOME/.local/share/applications"
+    fi
+    DESKTOPFILE="$APPSDIR/Olympus.desktop"
+    echo "- A desktop file will be created at $DESKTOPFILE"
+    echo "- The everest scheme handler will be registered for your user"
+    echo
+    echo "If you want to install Olympus system-wide, it is recommended to copy Olympus to /opt/olympus/, fix permissions and run install.sh as root."
+
+else
+    APPSDIR="/usr/share/applications"
+    DESKTOPFILE="/usr/share/applications/Olympus.desktop"
+    BINFILE="/usr/bin/olympus"
+    ICONFILE="/usr/share/icons/hicolor/128x128/apps/olympus.png"
+    echo "- olympus.desktop will be copied to $DESKTOPFILE"
+    echo "- A symlink to olympus.sh will be created at $BINFILE"
+    echo "- olympus.png will be copied to $ICONFILE"
+    echo "- The everest scheme handler will be registered for everyone"
+    echo
+    echo "Olympus will be installed system-wide. Please make sure that permissions are set up properly."
+fi
+
+echo
+read -p "Do you want to continue? y/N: " answer
+case ${answer:0:1} in
+    y|Y )
+    ;;
+    * )
+        exit 2
+    ;;
+esac
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Creating $DESKTOPFILE"
+    mkdir -p "$(dirname "$DESKTOPFILE")"
+    rm -f "$DESKTOPFILE"
+    cat "$DESKTOPSRCFILE" \
+    | sed "s/Exec=olympus/Exec=\"$(echo "$BINSRCFILE" | sed 's_/_\\/_g')\"/" \
+    | sed "s/Icon=olympus/Icon=$(echo "$ICONSRCFILE" | sed 's_/_\\/_g')/" \
+    > "$DESKTOPFILE"
+
+else
+    echo "Creating $DESKTOPFILE"
+    mkdir -p "$(dirname "$DESKTOPFILE")"
+    rm -f "$DESKTOPFILE"
+    cp "$DESKTOPSRCFILE" "$DESKTOPFILE"
+    chmod a+rx "$DESKTOPFILE"
+
+    echo "Creating $BINFILE"
+    mkdir -p "$(dirname "$BINFILE")"
+    rm -f "$BINFILE"
+    ln -s "$BINSRCFILE" "$BINFILE"
+    chmod a+rx "$BINFILE"
+
+    echo "Creating $ICONFILE"
+    mkdir -p "$(dirname "$ICONFILE")"
+    rm -f "$ICONFILE"
+    cp "$ICONSRCFILE" "$ICONFILE"
+    chmod a+r "$ICONFILE"
+fi
+
+echo "Registering everest scheme handler"
+xdg-mime default "$DESKTOPFILE" "x-scheme-handler/everest"
+echo "Updating desktop database"
+update-desktop-database "$APPSDIR"
+
+echo "Done!"
--- src/modinstaller.lua
+++ src/modinstaller.lua
@@ -40,53 +40,17 @@ function modinstaller.register()
         return false
 
     elseif userOS == "Linux" then
-        -- While we're here, might as well create a helpful .desktop
-        if config.currentrun == 0 then
-            return false
+        -- While we're here, might as well check if the everest scheme handler is registered.
+        local p = io.popen([["xdg-mime" "query" "default" "x-scheme-handler/everest"]])
+        local data = utils.trim(p:read("*a")) or ""
+        if p:close() and data == "" then
+            alert([[
+Olympus isn't fully installed.
+Please run install.sh to install the one-click installer handler.
+install.sh can be found in your Olympus installation folder.]])
         end
 
-        local srcdir = fs.getsrc()
-
-        local shpath = fs.joinpath(srcdir, "olympus.sh")
-        if not fs.isFile(shpath) then
-            return false
-        end
-
-        local iconpath = fs.joinpath(srcdir, "icon.png")
-        if not fs.isFile(iconpath) then
-            local fh = io.open(iconpath, "wb")
-            if fh then
-                fh:write(love.filesystem.read("data/icon.png"))
-                fh:close()
-            end
-        end
-
-        local appsdir = fs.joinpath(os.getenv("HOME"), ".local", "share", "applications")
-        if fs.isDirectory(appsdir) then
-            local desktoppath = fs.joinpath(appsdir, "Olympus.desktop")
-            if not fs.isFile(desktoppath) then
-                print("creating Olympus.desktop at", desktoppath)
-                local fh = io.open(desktoppath, "w+")
-                if fh then
-                    fh:write(string.format([[
-[Desktop Entry]
-Type=Application
-Terminal=false
-Categories=Game;
-Name=Olympus
-Exec="%s" %%u
-Icon=%s
-StartupNotify=false
-MimeType=x-scheme-handler/everest;
-]], shpath, iconpath))
-                    fh:close()
-                    print("registering everest url handler", pcall(os.execute, [["xdg-mime" "default" "]] .. desktoppath .. [[" "x-scheme-handler/everest"]]))
-                    print("updating desktop database", pcall(os.execute, [["update-desktop-database" "]] .. appsdir .. [["]]))
-                end
-            end
-        end
-
-        return true
+        return false
     end
 end
 
