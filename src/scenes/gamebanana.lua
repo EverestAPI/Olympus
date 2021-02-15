76739e6cebe1eed596442488520e925378280901
Jade Macho <nuda1998@gmail.com>
2021-02-15 14:02:45 +0100

    Improve detecting everest.yaml existence in GameBanana mod list

--- src/scenes/gamebanana.lua
+++ src/scenes/gamebanana.lua
@@ -514,19 +514,16 @@ function scene.item(info)
         return nil
     end
 
-    local file
-    for k, v in pairs(files) do
-        file = v
-        file._id = k
-        break
-    end
-
     local containsEverestYaml = false
-    if file and file._aMetadata and file._aMetadata._aArchiveFileTree then
-        for k, v in pairs(file._aMetadata._aArchiveFileTree) do
-            if v == "everest.yaml" then
-                containsEverestYaml = k
-                break
+
+    for id, file in pairs(files) do
+        if file and file._aMetadata and file._aMetadata._aArchiveFileTree then
+            for k, v in pairs(file._aMetadata._aArchiveFileTree) do
+                if v == "everest.yaml" then
+                    containsEverestYaml = true
+                    file._id = id
+                    break
+                end
             end
         end
     end
@@ -648,17 +645,7 @@ function scene.item(info)
                                 local btns = {}
 
                                 for _, file in pairs(files) do
-                                    local containsEverestYaml = false
-                                    if file and file._aMetadata and file._aMetadata._aArchiveFileTree then
-                                        for k, v in pairs(file._aMetadata._aArchiveFileTree) do
-                                            if v == "everest.yaml" then
-                                                containsEverestYaml = k
-                                                break
-                                            end
-                                        end
-                                    end
-
-                                    if containsEverestYaml then
+                                    if file and file._id then
                                         btns[#btns + 1] = uie.button(
                                             { { 1, 1, 1, 1 }, file._sFile, { 1, 1, 1, 0.5 }, " ∙ " .. os.date("%Y-%m-%d %H:%M:%S", file._tsDateAdded) .. " ∙ " .. uiu.countformat(file._nDownloadCount, "%d download", "%d downloads"), { 1, 1, 1, 0.5 }, "\n" .. file._sDescription},
                                             function(self)
