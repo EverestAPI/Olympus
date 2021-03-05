ba82b9fd727b45352c03cabdf12d04e53e39180a
Jade Macho <nuda1998@gmail.com>
2021-03-05 20:44:49 +0100

    Handle luacom returning nil on CreateObject

--- src/registry.lua
+++ src/registry.lua
@@ -1,9 +1,25 @@
+local sharp = require("sharp")
+
+local registry = {}
+
+function registry.getKeySharp(key)
+  return sharp._run("Win32RegGet", key)
+end
+registry.getKey = registry.getKeySharp
+
+function registry.setKeySharp(key, value)
+  return sharp._run("Win32RegSet", key, value)
+end
+registry.setKey = registry.setKeySharp
+
+
 local luacomStatus, luacom = pcall(require, "luacom")
 if luacomStatus and luacom then
-  local registry = {}
-
   function registry.getKey(key)
     local sh = luacom.CreateObject("WScript.Shell")
+    if not sh then
+      return registry.getKeySharp(key)
+    end
     local status, rv = pcall(sh.RegRead, sh, key)
     if status then
       return rv
@@ -14,25 +30,15 @@ if luacomStatus and luacom then
 
   function registry.setKey(key, value)
     local sh = luacom.CreateObject("WScript.Shell")
+    if not sh then
+      return registry.setKeySharp(key, value)
+    end
     if value == nil then
       return pcall(sh.RegDelete, sh, key)
     end
     return pcall(sh.RegWrite, sh, key, value)
   end
 
-  return registry
-end
-
-local sharp = require("sharp")
-
-local registry = {}
-
-function registry.getKey(key)
-  return sharp._run("Win32RegGet", key)
-end
-
-function registry.setKey(key, value)
-  return sharp._run("Win32RegSet", key, value)
 end
 
 return registry
