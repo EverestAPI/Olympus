433bf4def6cb6b191f1099faee19f7553af0988d
max4805 <max480@free.fr>
2023-01-21 21:05:04 +0100

    When a crash happens during an Olympus.Sharp task, display a "blue screen" instead of freezing

---
+++ sharp/CmdCrash.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Olympus {
+    public class CmdCrash : Cmd<bool> {
+        public override bool Run() {
+            throw new Exception("Crashed as requested!");
+        }
+    }
+}
