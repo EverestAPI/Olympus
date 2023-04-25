d8f488d26705923654d4e895a560f6e224a4f393
Popax21 <popa21@popa21.at>
2023-04-25 20:08:58 +0200

    Actually use the right concatenation operator

--- src/scenes/everest.lua
+++ src/scenes/everest.lua
@@ -323,8 +323,8 @@ function scene.load()
 
         if url then
             url = utils.trim(url)
-            if string:match(url, "?") then url = url + "&" else url = url + "?" end
-            url = url + "supportsNativeBuilds=true"
+            if string:match(url, "?") then url = url .. "&" else url = url .. "?" end
+            url = url .. "supportsNativeBuilds=true"
 
             buildsTask = utilsAsync.downloadJSON(url)
         end
