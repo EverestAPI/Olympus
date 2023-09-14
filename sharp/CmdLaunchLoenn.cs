7d5c244d0f6dbc9d896c1adc36d67e2d2d65111b
Wartori54 <toni.rigo.caldentey@gmail.com>
2023-09-14 23:07:35 +0200

    Fix loenn not launching if using system-wide love binaries

--- sharp/CmdLaunchLoenn.cs
+++ sharp/CmdLaunchLoenn.cs
@@ -13,19 +13,21 @@ namespace Olympus {
 
             if (PlatformHelper.Is(Platform.Windows)) {
                 loenn.StartInfo.FileName = Path.Combine(root, "Lönn.exe");
+				loenn.StartInfo.WorkingDirectory = root;
             } else if (PlatformHelper.Is(Platform.Linux)) {
-                // use Olympus's own love2d
-                loenn.StartInfo.FileName = Path.Combine(Program.RootDirectory, "love");
-                loenn.StartInfo.Arguments = "Lönn.love";
+                // use the find-love script that olympus also uses
+                loenn.StartInfo.FileName = Path.Combine(Program.RootDirectory, "find-love");
+                loenn.StartInfo.Arguments = Path.Combine(root, "Lönn.love");
                 loenn.StartInfo.UseShellExecute = true;
+				loenn.StartInfo.WorkingDirectory = Program.RootDirectory;
             } else {
                 // run the app
                 loenn.StartInfo.FileName = "open";
                 loenn.StartInfo.Arguments = "Lönn.app";
                 loenn.StartInfo.UseShellExecute = true;
+				loenn.StartInfo.WorkingDirectory = root;
             }
 
-            loenn.StartInfo.WorkingDirectory = root;
 
             Console.Error.WriteLine($"Starting Loenn process: {loenn.StartInfo.FileName} {loenn.StartInfo.Arguments} (in {root})");
 
