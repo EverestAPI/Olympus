ba6a296554420d5f8f016d0641fc401346d97589
Maddie <52103563+maddie480@users.noreply.github.com>
2026-09-14 22:37:44 +0200

    Some small UI scaling tweaks

--- src/conf.lua
+++ src/conf.lua
@@ -168,14 +168,16 @@ function love.conf(t)
     local config = require("config")
     config.load()
 
+    local scale = config.uiScale > 0 and config.uiScale or 1
+
     t.window.title = "Olympus"
     if love.system.getOS() ~= "OS X" then
         t.window.icon = "data/icon.png"
     end
-    t.window.width = 1100
-    t.window.minwidth = 1100
-    t.window.height = 600
-    t.window.minheight = 600
+    t.window.width = 1100 * config.uiScale
+    t.window.minwidth = 1100 * config.uiScale
+    t.window.height = 600 * config.uiScale
+    t.window.minheight = 600 * config.uiScale
     t.window.borderless = config.csd
     t.window.resizable = true -- when borderless, true causes a flickering border on Windows
     t.window.vsync = config.vsync and 1 or 0
